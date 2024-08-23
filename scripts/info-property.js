67a6d3c93e736709ed764466128385ee98e8b54d
alvaroMartinez13 <alvaro.martinezalcina13@gmail.com>
2024-08-23 13:57:28 -0500

    fix: :bug: Solución de Bugs
    Se solucionó unos bugs
    
    By @alvaroMartinez13

--- scripts/info-property.js
+++ scripts/info-property.js
@@ -1,32 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>KeyLife</title>
-    <link rel="stylesheet" href="../styles/info-inmueble.css">
-    <script type="module" defer src="../scripts/info-property.js"></script>
-</head>
-<body>
-    <header>
-        <picture>
-            <img id="logo" src="../images/logo-keylife.png" alt="">
+import { cargarDatos, cargarDatos_LocalStorage } from "./data.js";
 
-            <img id="sesion" src="../images/profile-user.png" alt="">
-        </picture>
+const properties = [];
 
-        <div class="escritorio">
-            <div class="container-escritorio">
-                <img src="../images/broken-heart.png" width="90vw" alt="">
-                <h2>MODO NO DISPONIBLE</h2>
-                <p>Uso exclusivo para dispositivos móviles</p>
-            </div>
-        </div>
+document.addEventListener("DOMContentLoaded", () => {
 
-    </header>
+    const id_properties = cargarDatos_LocalStorage("Property");
 
-    <main>
-        <section class="novedades">
+  cargarDatos("../data/properties.json").then((data) => {
+    for (let i of data.propierties) {
+      if (Object.keys(i).length !== 0 ) {
+        if(i.id_property === id_properties){
+            properties.push(i);
+        }
+      }
+    }
+  });
+  
+  rellenarTabla();
+
+});
+
+function rellenarTabla(){
+    const tabla = document.querySelector("main");
+
+    console.log(tabla);
+
+    console.log(properties);
+
+    let elemento = ``;
+
+    for(let property of properties){
+        elemento += `
+          <section class="novedades">
 
             <div class="contentItems">
 
@@ -53,8 +58,8 @@
         <section class="info-inmueble">
 
             <div class="titulo">
-                <h3>Apartamento</h3>
-                <h4>$300.000</h4>
+                <h3>${property.name_property}</h3>
+                <h4>${properties.price_property}</h4>
                 <a href="mensaje.html">
                     <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#F3F3F3"><path d="M320-520q17 0 28.5-11.5T360-560q0-17-11.5-28.5T320-600q-17 0-28.5 11.5T280-560q0 17 11.5 28.5T320-520Zm160 0q17 0 28.5-11.5T520-560q0-17-11.5-28.5T480-600q-17 0-28.5 11.5T440-560q0 17 11.5 28.5T480-520Zm160 0q17 0 28.5-11.5T680-560q0-17-11.5-28.5T640-600q-17 0-28.5 11.5T600-560q0 17 11.5 28.5T640-520ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/></svg>
                     <p>Contactar</p>
@@ -89,7 +94,7 @@
                                     <img src="../images/propietario.png" alt="">
                                 </picture>
                                 <span>
-                                    Lorena Sanchez
+                                    ${property.name_property}
                                 </span>
                             </div>
                         </td>
@@ -105,7 +110,7 @@
                                     <img src="../images/tipo.png" alt="">
                                 </picture>
                                 <span>
-                                    Arriendo
+                                    ${property.type_service}
                                 </span>
                             </div>
                         </td>
@@ -121,7 +126,7 @@
                                     <img src="../images/tipo.png" alt="">
                                 </picture>
                                 <span>
-                                    3
+                                    ${property.rooms_property}
                                 </span>
                             </div>
                         </td>
@@ -166,38 +171,7 @@
             </d>
 
         </section>
+        `;
 
-    </main>
-
-    <footer>
-        <nav>
-            <ul>
-                <li id="seleccionado">
-                    <a href="../pages/home.html">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="../pages/favorites.html">
-                        <img src="../images/hearts.png" width="24px" alt="">
-                    </a>
-                </li>
-
-                <li>
-                    <img src="../images/chat.png" width="24px" alt="">
-                </li>
-
-                <li>
-                    <img src="../images/shopping-bag_1410048.png" width="24px" alt="">
-                </li>
-
-                <li>
-                    <img src="../images/settings.png" width="24px" alt="">
-                </li>
-
-            </ul>
-        </nav>
-    </footer>
-</body>
-</html>
+    }
+}
